10546393070ca0d9baddcf77e72cd40f1cdfd13e
Pedro Vidal <pedrorvidal@gmail.com>
2025-01-13 18:26:25 -0300

    commit apenas docblock

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,4 +6,19 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    /**
+     * Function index
+     *
+     * @return null
+     */
+    /**
+     * Function index
+     *
+     * @return null
+     */
+    public function index()
+    {
+        dd('teste1');
+        die('teste2');
+    }
 }
