fc090ce732370ddac136224b97c57baea0cfac27
Pedro Vidal <pedrorvidal@gmail.com>
2025-01-29 12:58:25 -0300

    login tests and request object

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,9 @@ class AuthController extends Controller
     }
     public function loginSubmit(Request $request)
     {
-        dd($request->all());
-        echo "loginSubmit";
+        echo $request->input('text_username');
+        echo "<br>";
+        echo $request->input('text_password');
+        dd($request);
     }
 }
