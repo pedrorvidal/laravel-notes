30e483f079bea00ac31ce660aaff0416d1ffa297
Pedro Vidal <pedrorvidal@gmail.com>
2025-01-29 14:13:53 -0300

    Autentication custom messages and translations

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,16 +19,26 @@ class AuthController extends Controller
     {
         //Form Validation
         $request->validate(
+            //rules
             [
                 'text_username' => 'required|email',
-                'text_password' => 'required'
+                'text_password' => 'required|min:6|max:16'
+            ],
+            //error messages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'Username deve ser um email válido',
+                'text_password.required' => 'Password é obrigatório',
+                'text_password.min' => 'A Password deve ter pelo menos :min caracteres',
+                'text_password.max' => 'A Password deve ter no máximo :max caracteres',
             ]
         );
 
         //get user input
         $username = $request->input('text_username');
         $password = $request->input('text_password');
-
+        echo $username . "<br>";
+        echo $password . "<br>";
         echo "OK!";
     }
 }
