7fa1b0464831fdc6c7cb985d4839cad11bd0f38a
Pedro Vidal <pedrorvidal@gmail.com>
2025-02-03 23:53:08 -0300

    Force push para arrumar a aplicação quebrada

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\Route;
+
+//Auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+
+Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::get('/', [MainController::class, 'index']);
