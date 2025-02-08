a7a98331d2521c8362ea38aa3533c8cdac148d46
Pedro Vidal <pedrorvidal@gmail.com>
2025-02-07 21:39:05 -0300

    Intro Eloquent/relationships

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -9,7 +10,17 @@ class MainController extends Controller
     public function index()
     {
         // load user's notes
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
 
+        echo "<pre>";
+        print_r($user);
+        echo "</pre>";
+        echo "<pre>";
+        print_r($notes);
+        echo "</pre>";
+        die();
         // show home view
         return view('home');
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,4 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model {}
+class User extends Model
+{
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
+    }
+}
