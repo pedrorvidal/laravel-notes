b0fad04952f6675573f08c3875a5a0a888235d0a
Pedro Vidal <pedrorvidal@gmail.com>
2025-02-11 23:44:03 -0300

    encryption

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -20,5 +22,24 @@ class MainController extends Controller
     public function newNote()
     {
         echo "I'm creating a new note!";
+    }
+    public function editNote($id)
+    {
+        try {
+            $id = Crypt::decrypt($id);
+            echo "I'm editing note with id: $id";
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
+    }
+
+    public function deleteNote($id)
+    {
+        try {
+            $id = Crypt::decrypt($id);
+            echo "I'm deleting note with id: $id";
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
     }
 }
