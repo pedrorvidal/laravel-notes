5b230a794690c41cd54cc9b851413005baf89eae
Pedro Vidal <pedrorvidal@gmail.com>
2025-02-16 14:12:40 -0300

    Form validation + saving note
    Form validation, saving note in db, redirect home

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -24,11 +25,45 @@ class MainController extends Controller
     {
         // show new note view
         return view('new_note');
+
     }
     public function newNoteSubmit(Request $request)
     {
         // save new note
-        echo "I'm creating a new note";
+        // echo "I'm creating a new note";
+
+        // TODO: validate request
+        $request->validate(
+            //rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:6|max:3000'
+            ],
+            //error messages
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+            ]
+        );
+
+        // TODO: get user indexcreate new note
+        $id = session('user.id');
+
+        // TODO: get user indexcreate new note
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+
+        $note->save();
+        // TODO: redirect to home
+        return redirect()->route('home');
+
     }
     public function editNote($id)
     {
