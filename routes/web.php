3a634f93f8154f97f544c8720bc6134a41e65419
Pedro Vidal <pedrorvidal@gmail.com>
2025-02-15 23:17:06 -0300

    Form new note

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::middleware([CheckIsNotLogged::class])->group(function () {
 Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
+    Route::post('/newNoteSubmit', [MainController::class, 'newNoteSubmit'])->name('newNoteSubmit');
 
     //Edit note
     Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
